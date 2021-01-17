6d9b46d6896012c372e9eac281984a7d0975bba3
formegusto <formegusto@gmail.com>
2021-01-17 10:37:17 +0900

    join_host

--- server.js
+++ server.js
@@ -9,11 +9,37 @@ let io = socketio.listen(server);
 app.use(cors());
 const PORT = process.env.PORT || 8080;
 
-let users = {};
-
+let host = {};
 let socketToRoom = {};
 
 const maximum = process.env.MAXIMUM || 4;
+
+io.on('connection', socket => {
+    socket.on('join_host', data => {
+        console.log(`join_host ${data}`);
+
+        host[data.room] = {
+            id: socket.id,
+            isOpen: false
+        };
+
+        socketToRoom[socket.id] = data.room;
+
+        console.log(`host ==> ${host[data.room].id}`);
+        console.log(`socketToRoom ===> ${socketToRoom[socket.id] = data.room}`);
+    });
+
+    socket.on('disconnect', () => {
+        const roomID = socketToRoom[socket.id];
+        
+        // 연결 해제가 호스트 일 때,
+        if(host[roomID]){
+            if(host[roomID].id === socket.id) {
+
+            }
+        }
+    })
+});
 
 server.listen(PORT, () => {
     console.log(`server running on ${PORT}`);
